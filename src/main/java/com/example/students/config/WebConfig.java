a9211bd4c16267dc909393aa34416fcb42096492
Swaraj Jogi <125745551+swarajjogi@users.noreply.github.com>
2025-10-05 22:03:38 +0530

    Update WebConfig.java

--- src/main/java/com/example/students/config/WebConfig.java
+++ src/main/java/com/example/students/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://student-management-fronten.netlify.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
